4424bc54857ac32724cd32b047c843682b442e0f
yiahmed <yahyahmed28@gmail.com>
2023-05-16 12:20:59 -0700

    added hamburger menu to the navbar as well as responsiveness

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,17 +1,20 @@
 import Link from "next/link";
 import React from "react";
 import StyledLink from "./navbar-components/styledLink";
+import HamburgerMenu from "./navbar-components/hamburgerMenu";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Navbar = () => {
   return (
-    <div
-      style={{ height: "7vh" }}
-      className="flex items-center justify-center h-full w-full px-4 space-x-4 border bg-[#03001C] shadow-md"
-    >
-      <StyledLink href={"/"} text={"Home"} />
-      <StyledLink href={"/about"} text={"About"} />
-      <StyledLink href={"/project"} text={"Projects"} />
-      <StyledLink href={"/contact"} text={"Contact"} />
+    <div className="flex flex-wrap items-center justify-center px-4 space-x-4 border bg-[#03001C] shadow-md overflow-x-auto">
+      <div className="hidden sm:flex">
+        {/* StyledLink components will be visible on medium and large screens */}
+        <StyledLink href={"/"} text={"Home"} />
+        <StyledLink href={"/about"} text={"About"} />
+        <StyledLink href={"/project"} text={"Projects"} />
+        <StyledLink href={"/contact"} text={"Contact"} />
+      </div>
+      <HamburgerMenu />
     </div>
   );
 };
