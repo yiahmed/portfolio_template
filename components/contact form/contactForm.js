5d29973de27fdb431ebd13be0a17c4f7cdc66414
yahyawick <ya1yah2@gmail.com>
2023-06-04 13:05:25 -0700

    added sending functionality, few tweaks needed

--- components/contact form/contactForm.js
+++ components/contact form/contactForm.js
@@ -1,95 +1,133 @@
-import { Button } from "@mui/material";
-import React, { useState } from "react";
-import validator from "validator";
-// import Select from "react-select";
+import React, { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Button } from "@mui/material";
 
-// const validateData = (data) => {
-//   const errors = {};
+const ContactForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [isSubmitClicked, setIsSubmitClicked] = useState(false);
 
-//   // Perform your validation logic here using the 'validator' library or custom validation rules
-//   if (!validator.isEmail(data.email)) {
-//     errors.email = "Invalid email address";
-//   }
-//   return errors;
-// };
+  const onSubmit = async () => {
+    try {
+      await emailjs.sendForm(
+        "service_qpckw7m",
+        "template_l2cskae",
+        form.current,
+        "DIQZ5ce73oM6IPQS-"
+      );
+      toast.success("Email sent successfully");
+      // Do something after the email is sent, e.g., show a success message or redirect to another page
+    } catch (error) {
+      toast.error("Failed to send email");
+      console.error("Failed to send email:", error);
+      // Handle the error, e.g., show an error message to the user
+    }
+  };
 
-const ContactForm = ({ onSave, user = {} }) => {
-  const [userData, setUserData] = useState(user);
-  const [errors, setErrors] = useState({});
+  const form = useRef();
 
-  const { register, handleSubmit, control } = useForm();
-
-  // const handleSave = (data) => {
-  //   const errors = validateData(data);
-  //   if (Object.keys(errors).length > 0) {
-  //     setErrors(errors);
-  //     return;
-  //   }
-  //   onSave(data);
-  // };
+  const handleButtonClick = () => {
+    setIsSubmitClicked(true);
+  };
 
   return (
     <div className="h-full w-screen flex flex-col items-center justify-center">
-      <div class="w-1/3 h-3/4">
+      <div className="w-1/3 h-3/4">
         <form
-          onSubmit={handleSubmit((data) => {
-            console.log(data);
-          })}
+          onSubmit={handleSubmit(onSubmit)}
           className="bg-white h-full shadow-md rounded px-8 pt-6 pb-8 mb-4"
+          ref={form}
         >
-          <div class="mb-4">
+          <div className="mb-4">
             <div className="py-3">
               <label
-                class="block text-gray-700 text-sm font-bold mb-2"
-                for="username"
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="name"
               >
                 Name
               </label>
               <input
-                {...register("name", { required: true })}
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="username"
+                {...register("name", { required: "This is a required field" })}
+                className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                  isSubmitClicked && errors.name ? "border-red-500" : ""
+                }`}
+                id="name"
                 type="text"
                 placeholder="Please enter your name"
               />
+              {errors.name && isSubmitClicked && (
+                <p className="text-red-500 text-xs italic">
+                  {errors.name.message}
+                </p>
+              )}
             </div>
             <div className="py-3">
               <label
-                class="block text-gray-700 text-sm font-bold mb-2"
-                for="username"
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
               >
                 Email
               </label>
               <input
-                {...register("email", { required: true })}
-                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="username"
+                {...register("email", {
+                  required: "This is a required field",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: "Enter a valid email address",
+                  },
+                })}
+                className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                  isSubmitClicked && errors.email ? "border-red-500" : ""
+                }`}
+                id="email"
                 type="text"
                 placeholder="Please enter your email"
               />
+              {errors.email && isSubmitClicked && (
+                <p className="text-red-500 text-xs italic">
+                  {errors.email.message}
+                </p>
+              )}
             </div>
           </div>
-          <div class="mb-6 h-1/3">
-            <label class="block text-gray-700 text-sm font-bold mb-2">
+          <div className="mb-6 h-1/3">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
               Message
             </label>
             <textarea
-              {...register("message", { required: true, minLength: 10 })}
-              class="shadow appearance-none rounded w-full h-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              {...register("message", {
+                required: "This is a required field",
+                minLength: 10,
+              })}
+              className={`shadow appearance-none rounded w-full h-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline border ${
+                isSubmitClicked && errors.message ? "border-red-500" : ""
+              }`}
               placeholder="Your message here"
             />
+            {errors.message && isSubmitClicked && (
+              <p className="text-red-500 text-xs italic">
+                {errors.message.message}
+              </p>
+            )}
           </div>
-          <div class="flex items-center justify-center py-10">
+          <div className="flex items-center justify-center py-10">
             <button
-              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="submit"
+              onClick={handleButtonClick}
             >
               Submit
             </button>
           </div>
         </form>
       </div>
+      <ToastContainer />
     </div>
   );
 };
