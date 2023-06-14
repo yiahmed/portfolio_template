059bf48105392920c0b2050749423aa2aac7bbf0
yahyawick <ya1yah2@gmail.com>
2023-06-14 13:12:26 -0700

    added title to project page and few tweaks

--- pages/project/index.js
+++ pages/project/index.js
@@ -1,11 +1,18 @@
-import ProjectCards from "@/components/project cards/projectCards";
+import ProjectCards from "@/components/project page/projectCards";
 import GreetingContainer from "@/components/shared/container/greeting/greetingContainer";
 import React from "react";
 
 const index = () => {
   return (
-    <div className="w-full h-full flex items-center justify-center space-x-10 animate-fade">
-      <ProjectCards />
+    <div className="w-full h-full flex flex-col items-center justify-center animate-fade">
+      <h1 className="flex items-center justify-center h-12 text-5xl font-bold text-white mb-16">
+        Some things I've built
+      </h1>
+      <div className="flex space-x-10">
+        <ProjectCards />
+        <ProjectCards />
+        <ProjectCards />
+      </div>
     </div>
   );
 };
