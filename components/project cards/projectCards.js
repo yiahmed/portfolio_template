67a2be2f90faa18cdf018a3bf2ffc25aaf37f770
yahyawick <ya1yah2@gmail.com>
2023-06-13 17:01:23 -0700

    functionality complete

--- components/project cards/projectCards.js
+++ components/project cards/projectCards.js
@@ -6,28 +6,32 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Button, CardActionArea, CardActions } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
+import { Icon } from "@iconify/react";
 
-const yahyaImg = "/IMG_2584.jpg";
-const sportsApplication = "/sports-application.png";
 const projectInfo = [
   {
     title: "Sports Application",
     image: "/sports-application.png",
     description: "test description",
+    gitHubRepo: "https://github.com/yiahmed/counter-redux-typescript",
+    vercelLink: "https://sports-application.vercel.app/nba-postseason",
   },
 ];
 
 const ProjectCards = () => {
-  const [showIcons, setShowIcons] = useState(false);
-  const handleGitHubClick = () => {
-    window.open("https://github.com/yiahmed", "_blank");
+  const handleGitHubClick = (githubLink) => {
+    window.open(githubLink, "_blank");
+  };
+
+  const handleVercelClick = (vercelLink) => {
+    window.open(vercelLink, "_blank");
   };
 
   return (
     <>
       {projectInfo.map((project, index) => (
         <div key={index}>
-          <Card sx={{ maxWidth: 345 }}>
+          <Card sx={{ maxWidth: 345 }} className="bg-[#1e1e1e] text-white">
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -39,16 +43,22 @@ const ProjectCards = () => {
                 <Typography gutterBottom variant="h5" component="div">
                   {project.title}
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {project.description}
-                </Typography>
+                <Typography variant="body2">{project.description}</Typography>
               </CardContent>
             </CardActionArea>
             <CardActions>
-              <Button onClick={handleGitHubClick}>
+              <Button onClick={() => handleGitHubClick(project.gitHubRepo)}>
                 <GitHubIcon
-                  className={`text-3xl sm:text-3xl text-black  cursor-pointer`}
+                  className={`text-3xl sm:text-3xl text-white cursor-pointer`}
                   style={{ animationDelay: "1s" }}
+                />
+              </Button>
+              <Button onClick={() => handleVercelClick(project.vercelLink)}>
+                <img
+                  src="/vercel.svg"
+                  alt="next"
+                  width={75}
+                  className="filter-invert"
                 />
               </Button>
             </CardActions>
